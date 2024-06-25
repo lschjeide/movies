f87b544d33536b33bebe415897f400bb9f4e35b3
Leif Schjeide <leifschjeide@Leifs-MacBook-Pro.local>
2024-06-24 19:36:34 -0700

    get rid of local storage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 
-import { AuthProvider } from './contexts/authContext';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,14 +13,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-       <AuthProvider>
         <main>
           <Header/>
           <div className="h-screen-minus-120 md:h-screen-minus-80 bg-cover bg-center overflow-scroll" style={{ backgroundImage: 'url(https://images.ctfassets.net/arqv82dvpv8e/2AzVVZBCaRH5HocAWQcaQL/7acd6da399b895307f9347522f5098e4/VideoStill3.jpg?w=2880&fm=webp)' }}>
             {children}
           </div>
         </main>
-       </AuthProvider>
       </body>
     </html>
   );
